e7694d115e6250faddb15b3a2b8201b69bc7b451
prevner <dixy-prevner.m-poutou-babingui@estiam.com>
2023-07-27 01:13:44 +0200

    NavBar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,21 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode:"class",
   content: ['./*.html'],
   theme: {
-    extend: {},
-    colors:{
-      "mycolor":"#555"
+    extend: {
+      spacing:{
+        "big":"48rem"
+      }
+    },
+    screens:{
+      sm:"480px",
+      md:"768px",
+      lg:"1024px",
+      xl:"1280px",
+    },
+    fontFamily:{
+      nunito:['Nunito', 'sans-serif']
     }
   },
   plugins: [],
 }
-
