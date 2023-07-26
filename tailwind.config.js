13f58ca3d7d6accc841cfc8d5b55d1906895570e
prevner <dixy-prevner.m-poutou-babingui@estiam.com>
2023-07-26 23:40:14 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {},
+    colors:{
+      "mycolor":"#555"
+    }
+  },
+  plugins: [],
+}
+
